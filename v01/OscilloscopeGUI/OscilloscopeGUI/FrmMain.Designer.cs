d50657f3ddc073b5473050bdf5b9eeeb9c6f6d45
Gerard KESSE <tiakagerard@hotmail.com>
2024-11-06 19:00:11 +0100

    Création d’un projet C# Windows Forms dans Visual Studio

---
+++ v01/OscilloscopeGUI/OscilloscopeGUI/FrmMain.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace OscilloscopeGUI
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(429, 157);
+            Controls.Add(comboBox1);
+            Name = "FrmMain";
+            Text = "Oscilloscope GUI v1.1";
+            Load += FrmMain_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+    }
+}
