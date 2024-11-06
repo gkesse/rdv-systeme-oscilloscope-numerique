254b0a70b816fda6ee40b68e59ed12d058ba3100
Gerard KESSE <tiakagerard@hotmail.com>
2024-11-06 23:16:15 +0100

    Création d’une bibliothèque C# dans Visual Studio

---
+++ v01/OscilloscopeGUI/OscilloscopeGUI/Oscilloscope.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscilloscopeGUI
+{
+    internal class Oscilloscope
+    {
+        public static string[] GetResources()
+        {
+            string[] results = [
+                "Ressource 1",
+                "Ressource 2",
+                "Ressource 3"
+            ];
+            return results;
+        }
+    }
+}
---
+++ v02/OscilloscopeLIB/OscilloscopeLIB/Oscilloscope.cs
@@ -0,0 +1,13 @@
+
+public class Oscilloscope
+{
+    public static string[] GetResources()
+    {
+        string[] results = [
+            "Ressource 1",
+                "Ressource 2",
+                "Ressource 3"
+        ];
+        return results;
+    }
+}
